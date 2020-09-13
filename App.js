9aa244b0ea97b02c0433a68bc6f20706bb024f94
unknown <sachinbhatt669@gmail.com>
2020-09-13 16:49:02 +0530

    flex pratice, dummy home scrren

--- App.js
+++ App.js
@@ -14,21 +14,42 @@ import {
 export default function App() {
   const handleTestFunc = () => {
     // alert('Button Pressed');
-
-    return Alert.alert('title', 'Messege', [
-      {
-        text: 'Yes',
-        onPress: () => alert('Yes pressed'),
-      },
-      { text: 'No', onPress: () => alert('No pressed') },
-    ]);
   };
 
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
 
-      <Button title="Click Me" color="orange" onPress={handleTestFunc} />
+      <View style={{ flex: 8, width: '100%' }}>
+        <Image
+          style={{ width: '100%', height: '100%', zIndex: 1 }}
+          source={require('./assets/illustration.jpg')}
+        />
+
+        <View
+          style={{
+            position: 'absolute',
+            zIndex: 2,
+            flex: 1,
+            height: 400,
+            alignItems: 'center',
+            justifyContent: 'center',
+            top: 0,
+            right: 0,
+            left: 0,
+            // backgroundColor: '#fff',
+          }}
+        >
+          <Image
+            style={{ width: 100, height: 100, zIndex: 5 }}
+            source={require('./assets/favicon.png')}
+          />
+          <Text>dfasdfa</Text>
+        </View>
+      </View>
+
+      <View style={{ flex: 1, backgroundColor: '#fcb', width: '100%' }}></View>
+      <View style={{ flex: 1, backgroundColor: '#353', width: '100%' }}></View>
     </View>
   );
 }
@@ -36,13 +57,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ddd',
+    // flexDirection: 'row',
+    // alignItems: 'center',
     // paddingTop: '20px',
-    justifyContent: 'center',
-  },
-  image: {
-    width: '80%',
-    height: '30%',
+    // justifyContent: 'center',
   },
 });
